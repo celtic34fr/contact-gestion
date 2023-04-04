38e3870b3dfc582365c4391bac4f4022110c9b45
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-04-04 10:43:17 +0200

    test DI dans ContactWidget

--- src/Widget/ContactsWidget.php
+++ src/Widget/ContactsWidget.php
@@ -10,13 +10,14 @@ use Bolt\Widget\Injector\RequestZone;
 use Bolt\Widget\StopwatchAwareInterface;
 use Bolt\Widget\StopwatchTrait;
 use Bolt\Widget\TwigAwareInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ContactsWidget extends BaseWidget implements TwigAwareInterface, CacheAwareInterface, StopwatchAwareInterface
 {
     use CacheTrait;
     use StopwatchTrait;
 
-    public function __construct()
+    public function __construct(EntityManagerInterface $em)
     {
         $this->name = 'Contact Infos Widget';
         $this->target = ADDITIONALTARGET::WIDGET_BACK_DASHBOARD_ASIDE_TOP;
@@ -24,6 +25,7 @@ class ContactsWidget extends BaseWidget implements TwigAwareInterface, CacheAwar
         $this->template = '@contact-gestion/widget/infos.html.twig';
         $this->zone = RequestZone::FRONTEND;
         $this->cacheDuration = 0;
+        $this->em = $em;
     }
 
     public function run(array $params = []): ?string
