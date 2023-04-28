b80a8e76ee73b217d59c6e07e6d999ada3ef2894
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-04-28 13:40:08 +0200

    mise widget en bas de page adm + correction / docs

--- src/Widget/ContactsWidget.php
+++ src/Widget/ContactsWidget.php
@@ -3,24 +3,16 @@
 namespace Celtic34fr\ContactGestion\Widget;
 
 use Bolt\Widget\BaseWidget;
-use Bolt\Widget\CacheAwareInterface;
-use Bolt\Widget\CacheTrait;
 use Bolt\Widget\Injector\AdditionalTarget;
 use Bolt\Widget\Injector\RequestZone;
-use Bolt\Widget\StopwatchAwareInterface;
-use Bolt\Widget\StopwatchTrait;
 use Bolt\Widget\TwigAwareInterface;
-use Doctrine\ORM\EntityManagerInterface;
 
-class ContactsWidget extends BaseWidget implements TwigAwareInterface, CacheAwareInterface, StopwatchAwareInterface
+class ContactsWidget extends BaseWidget implements TwigAwareInterface
 {
-    use CacheTrait;
-    use StopwatchTrait;
-
     public function __construct()
     {
         $this->name = 'Contact Infos Widget';
-        $this->target = ADDITIONALTARGET::WIDGET_BACK_DASHBOARD_ASIDE_TOP;
+        $this->target = ADDITIONALTARGET::WIDGET_BACK_DASHBOARD_BOTTOM;
         $this->priority = 300;
         $this->template = '@contact-gestion/widget/infos.html.twig';
         $this->zone = RequestZone::FRONTEND;
