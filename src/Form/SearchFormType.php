eebee43dcfee27b5ffb61131ad075de18eac84e0
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-04-28 11:19:50 +0200

    correction errur déclaration

--- src/Form/SearchFormType.php
+++ src/Form/SearchFormType.php
@@ -2,13 +2,13 @@
 
 namespace Celtic34fr\ContactGestion\Form;
 
-use Celtic34fr\ContactGestion\FormEntity\SearchForm;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Celtic34fr\ContactGestion\FormEntity\SearchForm;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SearchFormType extends AbstractType
 {
