0291c6321ad71ccba184e41aaa452a4577a4d40e
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-06-11 20:27:13 +0200

    ajout maj list sur change input rename field

--- public/js/main.js
+++ public/js/main.js
@@ -5,32 +5,38 @@ new Sortable(document.querySelector("#origin"), {
     animation: 150,
 });
 
+function updateList() {
+    let cibles = document.querySelectorAll("#cible .list-group-item");
+    let list = document.querySelector("form .list input");
+    let newList = [];
+    cibles.forEach((cible) => {
+        let current = cible.querySelector("input");
+        newList.push({ index: current.name, value: current.value });
+    });
+    list.value = JSON.stringify(newList);
+    console.log(list, newList);
+}
+
 new Sortable(document.querySelector("#cible"), {
     group: "shared",
     animation: 150,
     onEnd: function (evt) {
-        let cibles = document.querySelectorAll("#cible .list-group-item");
-        let list = document.querySelector("form .list input");
-        let newList = [];
-        cibles.forEach((cible) => {
-            let current = cible.querySelector("input");
-            newList.push({ index: current.name, value: current.value });
-        });
-        list.value = JSON.stringify(newList);
-        console.log(list, newList);
+        updateList();
     },
     onSort: function (evt) {
-        let cibles = document.querySelectorAll("#cible .list-group-item");
-        let list = document.querySelector("form .list input");
-        let newList = [];
-        cibles.forEach((cible) => {
-            let current = cible.querySelector("input");
-            newList.push({ index: current.name, value: current.value });
-        });
-        list.value = JSON.stringify(newList);
-        console.log(list, newList);
+        updateList();
     },
 });
+
+let renames = document.querySelectorAll(".list-group-item .rename");
+renames.forEach(
+    (rename) =>
+        function (evt) {
+            rename.addEventListener("change", function (evt) {
+                updateList();
+            });
+        }
+);
 
 // gestion des évènement sur le drag and dropn choix chazmp + réorg + rename
 let cible = document.querySelector("#cible");
