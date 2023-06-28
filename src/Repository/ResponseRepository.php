61478a2bce809eac25d4c53e7993f980ad22fa09
Gilbert ARMENGAUD <gilbert.armengaud@gmail.com>
2023-06-28 16:27:19 +0200

    correction

--- src/Repository/ResponseRepository.php
+++ src/Repository/ResponseRepository.php
@@ -15,7 +15,7 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Response[]    findAll()
  * @method Response[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ResponsesRepository extends ServiceEntityRepository
+class ResponseRepository extends ServiceEntityRepository
 {
     use DbPaginateTrait;
 
